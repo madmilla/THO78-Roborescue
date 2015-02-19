a0903508e8b46d2cf623ba2ece8ddeedf4e863e1
Daniël van den Berg <danielvandenberg95@gmail.com>
2015-02-19 14:31:11 +0100

    Test for Jenkins

--- Daniel_van_den_Berg/MapSystem/main.cpp
+++ Daniel_van_den_Berg/MapSystem/main.cpp
@@ -7,13 +7,14 @@ int main(int argc, char *argv[])
 {
 
     std::cout << "Hello world, this is a testapplication!\n";
-    QApplication a(argc, argv);
-    if (QApplication::desktop()->screenCount() >= 1){
+    try{
+        QApplication a(argc, argv);
         std::cout << "This program ran on a pc with screen. \n";
         MainWindow w;
         w.show();
         return a.exec();
-    }else{
-        std::cout << "This program ran on a pc without screen. \n";
+    }catch(...){
+        std::cout << "This program ran on a pc without screen. (Or an exception occured for some other reason, I've got no idea.)\n";
+        return 0;
     }
 }
