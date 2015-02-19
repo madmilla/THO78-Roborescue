5334eb2d8f25055e4be31a8110115f0bd5789569
Daniël van den Berg <danielvandenberg95@gmail.com>
2015-02-19 14:24:30 +0100

    (Hopefully) created a way to detect screens
    This code (hopefully) can be used to detect if screens are available.
    Still have to test with Jenkins to make sure it works.

--- Daniel_van_den_Berg/MapSystem/main.cpp
+++ Daniel_van_den_Berg/MapSystem/main.cpp
@@ -1,15 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <iostream>
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
 
-    std::cout << "Hello world, this is a testapplication!";
-
+    std::cout << "Hello world, this is a testapplication!\n";
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+    if (QApplication::desktop()->screenCount() >= 1){
+        std::cout << "This program ran on a pc with screen. \n";
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }else{
+        std::cout << "This program ran on a pc without screen. \n";
+    }
 }
