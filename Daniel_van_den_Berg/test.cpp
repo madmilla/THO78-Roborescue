17676c372070cc3a3b2d194cdebf30b9321dc6cb
Daniël van den Berg <danielvandenberg95@gmail.com>
2015-02-21 15:13:56 +0100

    Test with Qt elements for Jenkins
    #3

--- Daniel_van_den_Berg/test.cpp
+++ Daniel_van_den_Berg/test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "MapSystem/mapeditor.h"
+#include <QStringList>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     MapEditor *mapEditor = new MapEditor();
-	std::cout << mapEditor->getAvailableMaps();
+	qDebug() << mapEditor->getAvailableMaps().join("\n");
 	delete mapEditor;
 	return 0;
 }
