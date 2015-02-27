52cc312127422b5f57c394dec405cd0b66f06d31
thijsvt <thijs.vantiem@student.hu.nl>
2015-02-27 17:38:08 +0100

    onnodige functie geprogrameerd en toen gui gebruikt

--- thijs_van_tiem/inwerken/main.cpp
+++ thijs_van_tiem/inwerken/main.cpp
@@ -4,37 +4,42 @@
 #include <QtCore>
 #include <iostream>
 
+int map(MainWindow &window){
+
+
+  QGridLayout *layout = new QGridLayout;
+
+
+    for(int i=0; i <20; i++){
+        for(int x=0; x<20; x++){
+            layout->addWidget(new QLabel("name"),i,x);
+            std::cout << i;
+            std::flush(std::cout);
+            std::cout << x;
+        }
+    }
+   //window.setLayout(layout);
+    QWidget * w = new QWidget;
+    w->setLayout(layout);
+    window.setCentralWidget(w);
+
+    return 0;
+}
+
+
+
 int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
     MainWindow window;
-    //QWidget *window2 = new QWidget;
-    //window2->setWindowTitle("Het inwerk project");
+
 
     window.setWindowTitle(QApplication::translate("het inwerk","Het inwerk project"));
 
-    //QGridLayout *layout = new QGridLayout;
-    //QLabel *label = new QLabel("Name:");
-    //QLineEdit * txtName = new QLineEdit;
-
-    //layout->addWidget(label,0,0);
-    //layout->addWidget(txtName,0,1);
-
-    //window2->setLayout(layout);
+    //map(window);
     window.resize(640,480);
     window.show();
-
-
-    //int i;
-    //int p=0;
-       //for(i =0; i <20; i++){
-           //layout->addWidget(label,i,p);
-           //p++;
-           //std::cout <<i;
-           //std::flush(std::cout);
-          //layout->addWidget(label,i,p);
-           //std::cout << p;
 
     return a.exec();
 }
