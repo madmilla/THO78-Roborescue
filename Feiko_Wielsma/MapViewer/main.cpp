66e8d1bf22926f246ad7417bb9ba1e8755687b86
Feiko Wielsma <feiko.wielsma@student.hu.nl>
2015-03-01 14:13:29 +0100

    Test

--- Feiko_Wielsma/MapViewer/main.cpp
+++ Feiko_Wielsma/MapViewer/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include "mapsystem.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     if(argc == 1 && argv[0] == "--test")
     {
+        std::cout << "Console\n";
         MapSystem * mapSystem = new MapSystem;
 
         delete mapSystem;
@@ -13,6 +15,7 @@ int main(int argc, char *argv[])
     }
     else
     {
+        std::cout << "Window\n";
         MapSystem * mapSystem = new MapSystem;
         QApplication a(argc, argv);
         MainWindow w (mapSystem);
