2476db1b29cf8fd0b149a170af1caf19574aa055
Biancakr <bianca.krieger@student.hu.nl>
2015-03-02 00:13:18 +0100

    sprint 0 opdracht test

--- Bianca Krieger/Map/testmap.cpp
+++ Bianca Krieger/Map/testmap.cpp
@@ -3,6 +3,20 @@
 
 int main(int argc, char *argv[]){
     Map *map = new Map();
+    map->set(8, 8, 2);
+    if(map->get(8, 8) != 2){
+        std::cout << "FAILURE :(";
+        return -1;
+    }
+    map->saveToFile("/tmp/testMapGridFileDingen");
     delete map;
+
+    Map *mappie = Map::loadFromFile("/tmp/testMapGridFileDingen");
+    if(map->get(8, 8) != 2){
+        std::cout << "FAILURE :(";
+        return -1;
+    }
+
+    std::cout << "SUCCESSSSS!!";
     return 0;
 }
