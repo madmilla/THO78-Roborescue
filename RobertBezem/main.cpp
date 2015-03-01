ba1436cc9c1a4caca4805b109b60b21421c6dee5
Robert Bezem <robert.bezem@gmail.com>
2015-03-01 11:49:24 +0100

    added noGUI option

--- RobertBezem/main.cpp
+++ RobertBezem/main.cpp
@@ -4,8 +4,15 @@
 
 #include "mainWindow.h"
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+    for(int i = 1; i<argcv ; i++){
+        if(argv[i] == "-noGUI"){
+            std::cout<<"noGUI enforced";
+            return EXIT_SUCCESS;
+        }
+    }
+
     QApplication app(argc, argv);
     mainWindow mainWin;
     mainWin.show();
