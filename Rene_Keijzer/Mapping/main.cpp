ef28fce9b42bb80dbc32eeeb12d5d5db9e1b0277
renekeijzer <rene.keijzer@casema.nl>
2015-03-01 22:37:06 +0100

    Final tests
    i hope

--- Rene_Keijzer/Mapping/main.cpp
+++ Rene_Keijzer/Mapping/main.cpp
@@ -4,22 +4,25 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc > 1 && argv[1] == "-t"){
+    if(argc > 1 && strcmp("-t", argv[1]) == 0){
         Map map("map.dat");
         if(map.width() != 20 && map.height() != 20){
             std::cout << "Map 20x20 Didn't load correctly";
+            return 1;
         }
 
-        Map map("map1.dat");
-        if(map.width() != 5 && map.height() != 5){
+        Map map1("map1.dat");
+        if(map1.width() != 5 && map1.height() != 5){
             std::cout << "Map 5x5 Didn't load correctly";
+            return 1;
         }
 
-        Map map("map2.dat");
-        if(map.width() != 0 && map.height() != 0){
+        Map map2("map2.dat");
+        if(map2.width() != 0 && map2.height() != 0){
             std::cout << "Map 0x0 Didn't load correctly";
+            return 1;
         }
-
+    return 0;
     }else{
 
     QApplication a(argc, argv);
