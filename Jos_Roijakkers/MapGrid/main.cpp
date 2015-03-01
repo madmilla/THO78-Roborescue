b28fb3a53ba40b752196d7899e76a5ad98f813f3
JosRoijakkers <Jos.Roijakkers@student.hu.nl>
2015-03-01 23:45:56 +0100

    QTupdate5.1

--- Jos_Roijakkers/MapGrid/main.cpp
+++ Jos_Roijakkers/MapGrid/main.cpp
@@ -4,6 +4,11 @@
 
 int main(int argc, char *argv[])
 {
+    if(argc > 1 && strcmp(argv[1], "test") == 0)
+    {
+        Test test;
+        return test.run();
+    }
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
