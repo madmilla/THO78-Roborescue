8c007959eb28ffcf512f40a3e81df5dbe4acd944
Kiplove <tijmenb@ziggo.nl>
2015-03-02 19:22:08 +0100

    Sprint_0 - commit 5

--- Tijmen_Bruggeman/Sprint_0/main.cpp
+++ Tijmen_Bruggeman/Sprint_0/main.cpp
@@ -5,10 +5,11 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc >=1 && argv[1]== "-test"){
-        std::cout << "this is a test";
+    if(argc >1 && strcmp(argv[1], "-test") == 0){
+        std::cout << "this is a tests\n";
     }
     else{
+        std::cout << "this is not a test\n";
         QApplication a(argc, argv);
         MainWindow w;
         qDebug() << "start";
