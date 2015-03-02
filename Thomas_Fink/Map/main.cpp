f21dcd2c1d3336946e70219e8e36e27924d8e4b4
Thomson2412 <Thomas.ipod16@gmail.com>
2015-03-02 23:44:30 +0100

    update main to start a test

--- Thomas_Fink/Map/main.cpp
+++ Thomas_Fink/Map/main.cpp
@@ -3,7 +3,7 @@
 #include "test.h"
 int main(int argc, char *argv[])
 {
-    if(argc > 1 && argv[1] == "-test" || true){
+    if(argc > 1 && strcmp(argv[1], "-test") == 0){
         Test test;
         return test.run();
     }else{
