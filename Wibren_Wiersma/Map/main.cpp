991407c63e0579638df0d0ca5806adb6383a35af
XibrenX <wibrenwiersma@hotmail.com>
2015-03-02 11:43:33 +0100

    GUI enabled again

--- Wibren_Wiersma/Map/main.cpp
+++ Wibren_Wiersma/Map/main.cpp
@@ -6,14 +6,14 @@
 
 int main(int argc, char *argv[])
 {
-    //if(argc > 1 && strcmp(argv[1], "-runtest") == 0){
+    if(argc > 1 && strcmp(argv[1], "-runtest") == 0){
         Tester tester;
         return tester.run();
-    //}else{
-        //QApplication app(argc,argv);
-        //MainWindow window;
-        //window.show();
-        //return app.exec();
-    //}
+    }else{
+        QApplication app(argc,argv);
+        MainWindow window;
+        window.show();
+        return app.exec();
+    }
 
 }
