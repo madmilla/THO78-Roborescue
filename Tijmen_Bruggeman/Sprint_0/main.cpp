ad5d41eb7ca00de2838c7e69430cfee6904ee2e7
Kiplove <tijmenb@ziggo.nl>
2015-03-02 19:12:27 +0100

    Sprint 0 - commit 4

--- Tijmen_Bruggeman/Sprint_0/main.cpp
+++ Tijmen_Bruggeman/Sprint_0/main.cpp
@@ -5,9 +5,14 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    qDebug() << "start";
-    w.show();
-    return a.exec();
+    if(argc >=1 && argv[1]== "-test"){
+        std::cout << "this is a test";
+    }
+    else{
+        QApplication a(argc, argv);
+        MainWindow w;
+        qDebug() << "start";
+        w.show();
+        return a.exec();
+    }
 }
