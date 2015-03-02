69e1b080f02a011263499afca77b506e035b7990
Danny Horvath <dannyhorvath@hotmail.com>
2015-03-02 19:33:28 +0100

    Test
    Test

--- Danny_Horvath/MapProgram/main.cpp
+++ Danny_Horvath/MapProgram/main.cpp
@@ -3,9 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+    if(argc >=1 && argv[1]== "-test"){
+        std::cout << "this is a test";
+    }
+    else{
+        QApplication a(argc, argv);
+        MainWindow w;
+        qDebug() << "start";
+        w.show();
+        return a.exec();
+    }
 }
--- Danny_Horvath/test.cpp
+++ Danny_Horvath/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "MapProgram/mainwindow.h"
 #include "MapProgram/mapdisplay.h"
 
 int main(int argc, char *argv[])
