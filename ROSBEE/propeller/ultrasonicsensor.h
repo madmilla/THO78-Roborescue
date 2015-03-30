abce7ca451b6e37360177aadd1183d61b3d58feb
Aesthy <edjekoek@hotmail.com>
2015-03-30 21:55:56 +0200

    Ultrasonic sensor class - doxygen fix

--- ROSBEE/propeller/ultrasonicsensor.h
+++ ROSBEE/propeller/ultrasonicsensor.h
@@ -1,13 +1,23 @@
+/**
+* @autor	Edwin Koek
+* @version	1.0
+* @since	30-03-2015
+*/
+
 #ifndef ULTRASONICSENSOR_H
 #define ULTRASONICSENSOR_H
 
+/**
+ * UltraSonicSensor class. 
+ * @brief This class creates objects that can be used to read data from a ultrasonic sensor.
+ */
 class UltraSonicSensor{
 public:
 /**
  * UltraSonicSensor constructor that sets the pin value and also the temperature. Currently
  * the temperature defaults to 22 as we have no means of actually measuring it yet.
  * 
- * @param int pin
+ * @param int pin specifies the pin that the sensor is connected to.
  */
   UltraSonicSensor(int pin);
 
@@ -16,6 +26,8 @@ public:
  * and a detected object in centimeters as a integer.
  * The distance calculation uses cm as distance unit and us as time unit therefor the standard
  * function is multiplied by 100 / 1000000
+ *
+ * @return Distance in cm
  */ 
   int getDistance();
   
