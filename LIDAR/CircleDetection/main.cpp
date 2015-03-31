8620356685dd7c553fc9baa5840eef166ed750e2
Kiplove <tijmenb@ziggo.nl>
2015-03-31 13:27:10 +0200

    Circle Detection

--- LIDAR/CircleDetection/main.cpp
+++ LIDAR/CircleDetection/main.cpp
@@ -10,8 +10,9 @@
 #include <math.h>
 #include <fstream>
 using namespace cv;
+//This function creates a .jpg file from a .txt file with pixels according to the output paramter.
 void makeImage(std::string source, std::string output = "output.jpg"){
-    std::ifstream file(source);
+    std::ifstream file(source); // open  inputfile
     if (!file.is_open()){
         printf("File niet gevonden.....\nProgramma wordt afgesloten\n");
         exit(-1);
@@ -19,22 +20,22 @@ void makeImage(std::string source, std::string output = "output.jpg"){
     std::string line;
     int imageHeight = 0;
     int imageWidth = 0;
-    while(getline(file,line)){
+    while(getline(file,line)){ //a while loop to get the image width and the imageHeight
         imageHeight++;
         if(line.length()> imageWidth){
             imageWidth = line.length();
         }
     }
-    Mat * newMat = new Mat(imageWidth,imageHeight,CV_8UC1);
+    Mat * newMat = new Mat(imageWidth,imageHeight,CV_8UC1); //Create a Mat object which will represent the image with all the pixels
     int x = 0;
     file.clear();
     file.seekg(0,file.beg);
-    while(getline(file,line)){
+    while(getline(file,line)){ //Walk through the file
         int y = 0;
         for(int i = 0; i< imageWidth; i++){
             char ch = line[i];
-            if(isdigit(ch)){
-                newMat->at<uchar>(Point(y,x)) = 255;
+            if(isdigit(ch)){ // if the char in the file is a number, the pixel should be 255
+                newMat->at<uchar>(Point(y,x)) = 255; // set the pixel at 255
             }
             else{
                 newMat->at<uchar>(Point(y,x)) = 0;
@@ -44,16 +45,17 @@ void makeImage(std::string source, std::string output = "output.jpg"){
         x++;
     }
     file.close();
-    imwrite(output, *newMat);
+    imwrite(output, *newMat); // save the image according to the output paramter
 }
+//This functions writes all the circle positions and radii to a file according to the outputName paramter
 void WriteCircles(CvSeq * circles, std::string outputName = "output.txt"){
-    std::ofstream output(outputName, std::ofstream::out);
+    std::ofstream output(outputName, std::ofstream::out); // open output
     if (!output.is_open()){
         printf("File niet gevonden.....\nProgramma wordt afgesloten\n");
         exit(-1);
     }
-    output << "(X,Y)    \tRadius\n";
-    for (size_t i = 0; i < circles->total; i++)
+    output << "(X,Y)    \tRadius\n"; //write data info to the file
+    for (size_t i = 0; i < circles->total; i++) // walk through the circles
     {
         // round the floats to an int
         float* p = (float*)cvGetSeqElem(circles, i);
@@ -65,13 +67,13 @@ void WriteCircles(CvSeq * circles, std::string outputName = "output.txt"){
     output.close();
 }
 void ShowCircles(CvSeq * circles, std::string sourceName = "output.jpg"){
-    IplImage * image;
+    IplImage * image; // image which will contain the loaded image
     if ((image = cvLoadImage("output.jpg"))== 0)
     {
         printf("File niet gevonden.....\nProgramma wordt afgesloten\n");
         exit(-1);
     }
-    for (size_t i = 0; i < circles->total; i++)
+    for (size_t i = 0; i < circles->total; i++) //Walk through all the circles
     {
         // round the floats to an int
         float* p = (float*)cvGetSeqElem(circles, i);
@@ -91,27 +93,29 @@ void ShowCircles(CvSeq * circles, std::string sourceName = "output.jpg"){
     cvShowImage("circles", image);
 }
 
+//This function detect circles and return a CVSEQ pointer with the detected circles
 CvSeq * DetectCircles(std::string sourceName){
     IplImage* img = NULL;
-    makeImage(sourceName); // transform .txt to .jpg file
-    if ((img = cvLoadImage("output.jpg"))== 0)
+
+    makeImage(sourceName, "output.jpg");//Make a image from the txt file and save it as output.jpg
+    if ((img = cvLoadImage("output.jpg"))== 0) //Check if the image can be loaded
     {
         printf("cvLoadImage failed\n");
     }
 
-    IplImage* gray = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1);
+    IplImage* gray = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1); // create a new image with only black and white pixels
     CvMemStorage* storage = cvCreateMemStorage(0);
 
-    cvCvtColor(img, gray, CV_BGR2GRAY);
+    cvCvtColor(img, gray, CV_BGR2GRAY); // transform the created image into the black/white image
 
-    // This is done so as to prevent a lot of false circles from being detected
-    cvSmooth(gray, gray, CV_GAUSSIAN, 7, 7);
 
-    IplImage* canny = cvCreateImage(cvGetSize(img),IPL_DEPTH_8U,1);
-    IplImage* rgbcanny = cvCreateImage(cvGetSize(img),IPL_DEPTH_8U,3);
-    cvCanny(gray, canny, 50, 100, 3);
+    cvSmooth(gray, gray, CV_GAUSSIAN, 7, 7); // This is done so as to prevent a lot of false circles from being detected
 
-    CvSeq* circles = cvHoughCircles(gray, storage, CV_HOUGH_GRADIENT, 1, gray->height/6, 145, 19);
+    IplImage* canny = cvCreateImage(cvGetSize(img),IPL_DEPTH_8U,1); // Create a image which will only contain the edges of the objects
+    IplImage* rgbcanny = cvCreateImage(cvGetSize(img),IPL_DEPTH_8U,3); // Create a image with color pixels where the circles will be drawn
+    cvCanny(gray, canny, 50, 100, 3); // Detect edges int he image
+
+    CvSeq* circles = cvHoughCircles(gray, storage, CV_HOUGH_GRADIENT, 1, gray->height/6, 145, 19); // Detect circles in the gray image
     return circles;
 }
 void test(){
