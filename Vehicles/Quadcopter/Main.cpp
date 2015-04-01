2a26972faf4959a6a6c2ac0830fcc25d7bf187a0
KjeldPerquin <kjeld.perquin@student.hu.nl>
2015-04-01 13:55:46 +0200

    Added PracticalSocket with corresponding Makefile

---
+++ Vehicles/Quadcopter/Main.cpp
@@ -0,0 +1,13 @@
+#include "Quadcopter.h"
+#include "PracticalSocket.h"
+
+int main()
+{
+	Quadcopter copter;
+	UDPSocket sock{ 15440 };
+	
+	while (1)
+	{
+		
+	}
+}
--- Vehicles/Quadcopter/Quadcopter.cpp
+++ Vehicles/Quadcopter/Quadcopter.cpp
@@ -2,7 +2,6 @@
 
 Quadcopter::Quadcopter()
 {
-
 }
 
 Quadcopter::~Quadcopter()
