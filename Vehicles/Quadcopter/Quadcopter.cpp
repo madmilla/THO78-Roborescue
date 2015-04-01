b2a16f13fa611a3e1bf049dbf1c0963f3213e10a
KjeldPerquin <kjeld.perquin@student.hu.nl>
2015-04-01 11:46:35 +0200

    Added initial quadcopter class files and added MAVLink dependencies

---
+++ Vehicles/Quadcopter/Quadcopter.cpp
@@ -0,0 +1,61 @@
+#include "Quadcopter.h"
+
+Quadcopter::Quadcopter()
+{
+
+}
+
+Quadcopter::~Quadcopter()
+{
+
+}
+
+void Quadcopter::liftOff()
+{
+
+}
+
+void Quadcopter::arm()
+{
+
+}
+
+void Quadcopter::moveLeft()
+{
+
+}
+
+void Quadcopter::moveRight()
+{
+
+}
+
+void Quadcopter::moveForward()
+{
+
+}
+
+void Quadcopter::moveBackward()
+{
+
+}
+
+void Quadcopter::stop()
+{
+
+}
+
+void Quadcopter::land()
+{
+
+}
+
+void Quadcopter::changeFlightSpeed(int)
+{
+
+}
+
+void Quadcopter::changeHeading(int)
+{
+
+}
