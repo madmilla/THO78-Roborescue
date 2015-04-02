f8c1a8bdb0a0a08ea60e15d13c9b2c948a80aa12
Daniel van den Berg <danielvandenberg95@student.hu.nl>
2015-04-02 13:19:49 +0200

    Changed stuff to Main.cpp

--- Vehicles/Quadcopter/Main.cpp
+++ Vehicles/Quadcopter/Main.cpp
@@ -1,12 +1,17 @@
 #include "Quadcopter.h"
-#include "SerialPort.h"
-#include <string>
+#include "SerialPort.h" 
+#include <unistd.h>
+#include <iostream>
 
-int main()
-{
-	SerialPort port{ "COM4" };
-	Quadcopter copter{ port };
-	while (1)
-	{
+int main() {
+	SerialPort serialPort("/dev/ttyUSB0");
+	Quadcopter quadcopter(serialPort);
+	while (1){
+		std::cout << "moveLeft\n";
+		quadcopter.moveLeft (400);
+		sleep(300000);
+		std::cout << "moveRight\n";
+		quadcopter.moveRight (400);
+		sleep(300000);
 	}
 }
