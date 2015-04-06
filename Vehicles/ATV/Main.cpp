4b143f8be57f8334d779758f18f8e8223213b54f
yorricklans <yorricklans@hotmail.com>
2015-04-06 14:12:14 +0200

    changes

--- Vehicles/ATV/Main.cpp
+++ Vehicles/ATV/Main.cpp
@@ -2,16 +2,34 @@
 #include "SerialPort.h"
 #include "TempMAVSender.h"
 #include "ATV.h"
+#include <iostream>
+#include <cstdlib>
+#include <thread>
 
+void checkforexit()
+{
+	std::string chars;
+	while (1)
+	{
+		std::cin >> chars;
+		if (chars == "exit")
+		{
+			exit(0);
+		}
+	}
+}
 
 int main()
 {
-	SerialPort port{ "COM9" };
+	SerialPort port{ "COM4" };
 	TempMAVSender mavlinkSender{ port };
 	ATV atv{ mavlinkSender };
-	
+	atv.emergencyStop();
+	std::thread th1(checkforexit);
+
 	while (1)
 	{
-		atv.moveForward();
+		//atv.turnLeft(100);
+		//atv.moveForward(20);
 	}
 }
