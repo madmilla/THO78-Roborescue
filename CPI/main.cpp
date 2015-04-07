6db6bd6b8cbf22b27e01591608f715d75fb90dfb
renekeijzer <rene.keijzer@casema.nl>
2015-04-07 11:26:09 +0200

    Added some socket work

--- CPI/main.cpp
+++ CPI/main.cpp
@@ -1,8 +1,11 @@
-#include "SerialUDPSocket.hpp"
+#include <windows.h>
+#include "SerialUDPSocket.h"
 int main(int argc, char * argv[]){
-	SerialUDPSocket sock(argv[0], argv[1]);
+	std::string  addr = argv[1];
+	SerialUDPSocket sock(atoi(argv[0]), addr);
 	while (1){
-	
+		sock.writeData("test", sizeof("test"));
+		Sleep(3000);
 	}
 
 }
