23ad970a37eb527b23e6cc640cabf11873a2a738
renekeijzer <rene.keijzer@casema.nl>
2015-04-08 13:00:33 +0200

    Client updated

--- CPI/receiver/receiver/client.cpp
+++ CPI/receiver/receiver/client.cpp
@@ -38,32 +38,22 @@ void client::sendMessage(){
 	std::string letter = "1";
 	while (1){
 		std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-		//printf("Enter message: ");
-		//gets_s(message);
-
-		//send the message
 		if (sendto(s, letter.c_str(), strlen(letter.c_str()), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
 		{
 			printf("sendto() failed with error code : %d", WSAGetLastError());
 
 		}
 
-		//receive a reply and print it
-		//clear the buffer by filling null, it might have previously received data
+
 		memset(buf, '\0', BUFLEN);
-		//try to receive some data, this is a blocking call
 		if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == SOCKET_ERROR)
 		{
 			printf("recvfrom() failed with error code : %d", WSAGetLastError());
-			//exit(EXIT_FAILURE);
 		}
 		
 		puts(buf);
-		//std::cout << "dit is de buf"  << *buf;
 		i = atoi(buf) + 1;
-		//std::cout << "dit is i:" << i<<'\n';
 		letter = std::to_string(i);
-		//std::cout<< "dit is i:" << i;
 	}
 
 	closesocket(s);
