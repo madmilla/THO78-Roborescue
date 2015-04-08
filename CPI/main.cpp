94d255c5aa8cc33cd20b55221739fbc9bfd17bd0
renekeijzer <rene.keijzer@casema.nl>
2015-04-08 11:00:10 +0200

    Merge eroor

--- CPI/main.cpp
+++ CPI/main.cpp
@@ -1,14 +1,18 @@
 #include <string>
 #include <cstdlib>
 #include "include/SerialUDPSocket.h"
+  #include <windows.h>
+  #include <winsock2.h>
+  #include <ws2tcpip.h>
+
 int main(int argc, char * argv[]){
 	
 	std::string  addr = argv[1];
 	SerialUDPSocket sock(atoi(argv[0]), addr);
-	std::string message = "test";
+	unsigned char x = 'x';
 	while (1){
 
-		sock.writeData(&message.c_str(), sizeof(message));
+		sock.writeData(&x, sizeof(x));
 	}
 
 }
