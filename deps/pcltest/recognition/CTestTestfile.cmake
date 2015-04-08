cfa4998a3d3a65d4582d8ec95f85a991462a38a0
Owantjaaa <owen.hoogenboezem@student.hu.nl>
2015-04-08 12:11:32 +0200

    pcl test

---
+++ deps/pcltest/recognition/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/owen/pcl/src/pcl-pcl-1.7.2/recognition
+# Build directory: /home/owen/test/recognition
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ deps/pcltest/segmentation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/owen/pcl/src/pcl-pcl-1.7.2/segmentation
+# Build directory: /home/owen/test/segmentation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
