b4150b1e3c223266cac6f6f9f168909fefc24006
Owantjaaa <owen.hoogenboezem@student.hu.nl>
2015-04-08 10:45:30 +0200

    smaller pcl

---
+++ deps/pcl/io/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/owen/pcl/src/pcl-pcl-1.7.2/io
+# Build directory: /home/owen/test/io
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
