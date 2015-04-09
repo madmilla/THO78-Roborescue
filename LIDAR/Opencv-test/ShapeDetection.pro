42af7b60a634c2cf3868eadb8d696bc472a1c33c
Kiplove <tijmenb@ziggo.nl>
2015-04-09 13:26:47 +0200

    no message

---
+++ LIDAR/Opencv-test/ShapeDetection.pro
@@ -0,0 +1,40 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-03-26T16:50:13
+#
+#-------------------------------------------------
+
+
+TARGET = ShapeDetection
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    shapedetector.cpp \
+    main.cpp
+win32 {
+    INCLUDEPATH += "..\..\deps\files\opencv" \
+
+    CONFIG(debug,debug|release) {
+        LIBS += -L"..\\..\\deps\\libs\\opencv\\windows" \
+            -lopencv_core2410d \
+            -lopencv_highgui2410d \
+            -lopencv_imgproc2410d \
+
+    }
+
+    CONFIG(release,debug|release) {
+        DEFINES += QT_NO_WARNING_OUTPUT QT_NO_DEBUG_OUTPUT
+        LIBS += -L"..\\..\\deps\\libs\\opencv\\windows" \
+            -lopencv_core2410 \
+            -lopencv_highgui2410 \
+            -lopencv_imgproc2410 \
+
+    }
+}
+
+HEADERS += \
+    shapedetector.h
---
+++ LIDAR/Opencv-test/main.cpp
@@ -0,0 +1,42 @@
+
+// First parameter is .txt file with the pixels.
+
+// compiled with: g++ circles.cpp -o circles `pkg-config --cflags --libs opencv`
+#include <iostream>
+#include <stdio.h>
+#include "time.h"
+#include "ctime"
+#include <math.h>
+#include <fstream>
+#include "shapedetector.h"
+#include "imgproc/imgproc.hpp"
+#include "highgui/highgui.hpp"
+#include "core/core.hpp"
+
+void test(){
+    ShapeDetector sD;
+    clock_t Start = clock();
+    CvSeq* circles = sD.detectCircles("circles400.txt");
+    clock_t end = clock();
+    int time = end - Start;
+    printf("%d circles detected in %f sec\n\n", circles->total,(float)time /CLOCKS_PER_SEC);
+    if (circles->total != 7){
+        exit(-1);
+    }
+    sD.writeCircles(circles);
+}
+
+int main(int argc, char** argv)
+{
+    if(argc >1 && strcmp(argv[1], "test") == 0){
+        printf("Testing CircleDetection\n\n");
+        test();
+        return 0;
+    }
+    ShapeDetector sD;
+    CvSeq* circles = sD.detectCircles(argv[1]);
+    sD.writeCircles(circles);
+    sD.showCircles(circles);
+    //cv::waitKey(0);
+    return 0;
+}
