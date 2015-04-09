4f44eaf105373961b79e6257f979967c290bb9f6
thijsvt <thijs.vantiem@student.hu.nl>
2015-04-09 10:07:16 +0200

    comments

--- CPI-UI/lidar.h
+++ CPI-UI/lidar.h
@@ -3,17 +3,31 @@
 
 #include <string>
 
+/// this class is here till the lidar class has been made.
+
 class lidar
 {
 public:
     lidar();
     ~lidar();
 
+
+    /// initialisation Lidar
     unsigned int init();
+
+    /// Set the hz of the lidar every 30rpm is 0.5 hz
     int setRPM(int rpm);
+
+    /// start the lidar
     void startLidar();
+
+    /// stop the lidar
     void stopLidar();
+
+    /// get the status of the lidar (connected or not
     std::string getStatus();
+
+    /// get the rpm status of the class
     int rpmStatus();
 
 
--- CPI-UI/lidarwindow.h
+++ CPI-UI/lidarwindow.h
@@ -7,6 +7,13 @@
 #include <QTimer>
 #include "lidar.h"
 
+/**
+* @class   lidarwindow
+* @author Thijs van Tiem  (thijsvantiem@gmail.com)
+* @date   April, 2015
+* @brief  the class that handles the lidarwindow and the buttons on it.
+*/
+
 namespace Ui {
 class lidarwindow;
 }
@@ -16,7 +23,16 @@ class lidarwindow : public QMainWindow
     Q_OBJECT
 
 public:
+    /**
+    *   Creates the lidar object en sets the qwidget parent
+    *	@param lidar * l create the lidar object
+    *   @param QWidget * Parent tells if there are parents or not. there are no parents here.
+    */
     explicit lidarwindow(lidar * l, QWidget *parent = 0);
+
+    /**
+    *   deconstructor lidarwindow();
+    */
     ~lidarwindow();
 
 private slots:
