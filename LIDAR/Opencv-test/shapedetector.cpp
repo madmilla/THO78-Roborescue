331990745b26482356887fe99eb2b504a4e2ba85
Kiplove <tijmenb@ziggo.nl>
2015-04-09 17:49:51 +0200

    no message

--- LIDAR/Opencv-test/shapedetector.cpp
+++ LIDAR/Opencv-test/shapedetector.cpp
@@ -9,24 +9,78 @@ ShapeDetector::~ShapeDetector()
 
 }
 
-using namespace cv;
-//This function creates a .jpg file from a .txt file with pixels according to the output paramter.
-void ShapeDetector::makeImage(std::string source, std::string output){
+void ShapeDetector::writeCirclesToConsole(const CvSeq * circles){
+    std::cout << "found circles: " << (circles->total) << std::endl << std::endl;
+    for (int i = 0; i < circles->total; i++) // walk through the circles
+    {
+        // round the floats to an int
+        std::cout << "Circle "<< i+1 << std::endl;
+        float* p = (float*)cvGetSeqElem(circles, i);
+        cv::Point center(cvRound(p[0]), cvRound(p[1]));
+        int radius = cvRound(p[2]);
+        std::cout << "("<< center.x << "," << center.y<< ")    \t" << radius << "\n\n";
+    }
+}
+
+void ShapeDetector::drawCircles(const CvSeq * circles, Mat & image){
+    for (int i = 0; i < circles->total; i++) //Walk through all the circles
+    {
+        // round the floats to an int
+        float* p = (float*)cvGetSeqElem(circles, i);
+        cv::Point center(cvRound(p[0]), cvRound(p[1]));
+        int radius = cvRound(p[2]);
+
+        // draw the circle center
+        circle(image, center, CIRCLE_CENTER_RADIUS, CIRCLE_CENTER_COLOR, CENTER_THICKNESS, CIRCLE_LINE_TYPE);
+        // draw the circle outline
+        circle(image, center, radius, CIRCUMFERENCE_COLOR, CIRCLE_THICKNESS, CIRCLE_LINE_TYPE);
+    }
+    //Show Image with the detected circles
+}
+
+CvSeq * ShapeDetector::detectCircles(const Mat & image){
+    IplImage img = image;
+
+    IplImage* gray = cvCreateImage(cvGetSize(&img), IPL_DEPTH_8U, 1); // create a new image with only black and white pixels
+    CvMemStorage* storage = cvCreateMemStorage(0);
+
+    cvCvtColor(&img, gray, CV_BGR2GRAY); // transform the created image into the black/white image
+
+    cvSmooth(gray, gray, CV_GAUSSIAN, SMOOTH, SMOOTH); // This is done so as to prevent a lot of false circles from being detected
+
+    IplImage* canny = cvCreateImage(cvGetSize(&img),IPL_DEPTH_8U,1); // Create a image which will only contain the edges of the objects
+    cvCanny(gray, canny, EDGE_TRESHHOLD, EDGE_TRESHHOLD); // Detect edges int he image
+
+    CvSeq* circles = cvHoughCircles(gray, storage, CV_HOUGH_GRADIENT, RESOLUTION_INVERSERATIO, gray->height/MIN_DISTANCE_CIRCLES, EDGE_TRESHHOLD*3, CIRCLE_CENTER_TRESHHOLD); // Detect circles in the gray image
+    return circles;
+}
+
+bool ShapeDetector::callCvSmooth(const Mat & m_src, const Mat & m_dest, const int smooth_type, const int param1, const int param2) {
+    if(m_src.empty()) {
+        return false;
+    }
+    IplImage src = m_src;
+    IplImage dst = m_dest;
+    cvSmooth( &src, &dst, smooth_type, param1, param2 );
+    return true;
+}
+
+Mat ShapeDetector::createImage(const std::string & source){
     std::ifstream file(source); // open  inputfile
     if (!file.is_open()){
-        printf("File niet gevonden.....\nProgramma wordt afgesloten\n");
-        exit(-1);
+        std::cout << "could not open file with name " << source << std::endl;
+        exit(EXIT_FAILURE);
     }
     std::string line;
     size_t imageHeight = 0;
     size_t imageWidth = 0;
-    while(getline(file,line)){ //a while loop to get the image width and the imageHeight
+    while(getline(file,line)){ //a while loop to get the image width and the image Height
         imageHeight++;
         if(line.length()> imageWidth){
             imageWidth = line.length();
         }
     }
-    Mat * newMat = new Mat((int)imageWidth,(int)imageHeight,CV_8UC1); //Create a Mat object which will represent the image with all the pixels
+    Mat mat((int)imageWidth,(int)imageHeight,CV_8UC1); //Create a Mat object which will represent the image with all the pixels
     int x = 0;
     file.clear();
     file.seekg(0,file.beg);
@@ -35,86 +89,103 @@ void ShapeDetector::makeImage(std::string source, std::string output){
         for(int i = 0; i< (int)imageWidth; i++){
             char ch = line[i];
             if(isdigit(ch)){ // if the char in the file is a number, the pixel should be 255
-                newMat->at<uchar>(Point(y,x)) = black_pixel; // set the pixel at 255 ==
+                mat.at<uchar>(Point(y,x)) = BLACK_PIXEL; // set the pixel at 255
             }
             else{
-                newMat->at<uchar>(Point(y,x)) = white_pixel;
+                mat.at<uchar>(Point(y,x)) = WHITE_PIXEL;
             }
             y++;
         }
         x++;
     }
     file.close();
-    imwrite(output, *newMat); // save the image according to the output paramter
+    imwrite("output.jpg", mat); // save the image
+
+    return Mat(imread("output.jpg")); //read and return the image
 }
-//This functions writes all the circle positions and radii to a file according to the outputName paramter
-void ShapeDetector::writeCircles(CvSeq * circles, std::string outputName){
-    std::ofstream output(outputName, std::ofstream::out); // open output
-    if (!output.is_open()){
-        printf("File niet gevonden.....\nProgramma wordt afgesloten\n");
+
+void ShapeDetector::checkLines(vector<Vec4i> & lines) {
+
+    for( size_t i = 0; i < lines.size(); i++ ) {    //walk through the line container
+        Vec4i second = lines[i];
+        for (size_t j = 0; j < lines.size(); j++) {
+            Vec4i current = lines[j];
+
+            const int current_x1 = (current[0]);
+            const int current_y1 = (current[1]);
+            const int current_x2 = (current[2]);
+            const int current_y2 = (current[3]);
+
+            const int second_x1 = second[0];
+            const int second_y1 = second[1];
+            const int second_x2 = second[2];
+            const int second_y2 = second[3];
+
+            if((second_x1 - RANGE_CHECK)< current_x1&& (second_x2 + RANGE_CHECK) > current_x2) {
+                if(((second_y1 < (current_y1 + RANGE_CHECK)) && second_y1 > (current_y1 - RANGE_CHECK)) &&  //compare the current and the second line
+                  ((second_y2 < (current_y2 + RANGE_CHECK)) && second_y2 > (current_y2 - RANGE_CHECK))){    // coordinates in a range
+
+                    if(j!=i){
+                        lines.erase(lines.begin()+j); // erase the target line
+                    }
+                }
+            }
+        }
+    }
+}
+
+vector<Vec4i> ShapeDetector::searchLines(const Mat & image) {
+
+    if (image.empty()) { //check if there is a image
+        std::cout << "could not read image" << std::endl;
         exit(-1);
     }
-    output << "(X,Y)    \tRadius\n"; //write data info to the file
-    for (int i = 0; i < circles->total; i++) // walk through the circles
-    {
-        // round the floats to an int
-        float* p = (float*)cvGetSeqElem(circles, i);
-        cv::Point center(cvRound(p[0]), cvRound(p[1]));
-        int radius = cvRound(p[2]);
-        output << "("<< center.x << "," <<center.y<< ")\t" << radius << "\n";
-        std::cout << "("<< center.x << "," << center.y<< ")     \t" << radius << "\n";
+
+    Mat dest;
+    if(!callCvSmooth(image, image, CV_GAUSSIAN, SMOOTH, SMOOTH)) {
+       std::cout << "the source file is empty!" << std::endl;
+       exit(-1);
     }
-    output.close();
-}
-//This function shows the input image with the detected circles;
-void ShapeDetector::showCircles(CvSeq * circles, std::string sourceName ){
-    IplImage * image; // image which will contain the loaded image
-    if ((image = cvLoadImage("output.jpg"))== 0)
-    {
-        printf("File niet gevonden.....\nProgramma wordt afgesloten\n");
-        exit(-1);
-    }
-    for (int i = 0; i < circles->total; i++) //Walk through all the circles
-    {
-        // round the floats to an int
-        float* p = (float*)cvGetSeqElem(circles, i);
-        cv::Point center(cvRound(p[0]), cvRound(p[1]));
-        int radius = cvRound(p[2]);
+    Canny(image, dest, CANNY_THRESHHOLD1, CANNY_THRESHHOLD2); //extracts the egdes of an image
 
+    vector<Vec4i> lines;  // container to save te lines
+    HoughLinesP(dest, lines, HOUGHLINES_RHO, HOUGHLINES_THETA, HOUGHLINES_THRESHHOLD,
+    HOUGHLINES_MINLINELENGTH, HOUGHLINES_MAXLINEGAP);  //search the lines
 
-        // draw the circle center
-        cvCircle(image, center, circleCenterRadius, circleCenterColor, centerThickness, circleLineType);
+    checkLines(lines); //check for double lines
 
-        // draw the circle outline
-        cvCircle(image, center, radius, circumferenceColor, circleThickness, circleLineType);
-    }
-
-    ///Show Image with the detected circles
-    cvNamedWindow("circles");
-    cvShowImage("circles", image);
+    return lines;  // return the saved lines
 }
 
-//This function detect circles and return a CVSEQ pointer with the detected circles
-CvSeq * ShapeDetector::detectCircles(std::string sourceName){
-    IplImage* img = NULL;
-
-    makeImage(sourceName, "output.jpg");//Make a image from the txt file and save it as output.jpg
-    if ((img = cvLoadImage("output.jpg"))== 0) //Check if the image can be loaded
-    {
-        printf("cvLoadImage failed\n");
+void ShapeDetector::writeLinesToConsole(const vector<Vec4i> & lines) {
+    std::stringstream stream;
+    stream << "found lines: " << lines.size() << std::endl << std::endl;
+    for (size_t i = 0; i < lines.size(); i++ ) {
+        Vec4i line = lines[i];
+        stream << "line " << i+1 << std::endl;
+        stream << "(x1, y1) (" << line[0] << ',' << line[1] << ')' << std::endl;
+        stream << "(x2, y2) (" << line[2] << ',' << line[3] << ')' << std::endl << std::endl;
     }
-
-    IplImage* gray = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1); // create a new image with only black and white pixels
-    CvMemStorage* storage = cvCreateMemStorage(0);
-
-    cvCvtColor(img, gray, CV_BGR2GRAY); // transform the created image into the black/white image
-
-
-    cvSmooth(gray, gray, CV_GAUSSIAN, smooth, smooth); // This is done so as to prevent a lot of false circles from being detected
-
-    IplImage* canny = cvCreateImage(cvGetSize(img),IPL_DEPTH_8U,1); // Create a image which will only contain the edges of the objects
-    cvCanny(gray, canny, edgeTreshhold, edgeTreshhold); // Detect edges int he image
-
-    CvSeq* circles = cvHoughCircles(gray, storage, CV_HOUGH_GRADIENT, resolutionInverseRatio, gray->height/minDistanceCircles, edgeTreshhold*3, circleCenterTreshhold); // Detect circles in the gray image
-    return circles;
+    std::cout << stream.str();
 }
+
+void ShapeDetector::drawLines(const vector<Vec4i> & lines, Mat & final_dest) {
+    for( size_t i = 0; i < lines.size(); i++ ) {
+        Vec4i l = lines[i];
+        line( final_dest, Point(l[0], l[1]), Point(l[2], l[3]), LINECOLOR, THICKNESS, CV_AA);
+    }
+}
+
+void ShapeDetector::writeObjectsToConsole(const vector<Vec4i> & lines, const CvSeq * circles) {
+    writeCirclesToConsole(circles);
+    writeLinesToConsole(lines);
+}
+
+void ShapeDetector::showObjects(const vector<Vec4i> & lines, const CvSeq * circles, const Mat & orginal_image, Mat & custom_image){
+    drawLines(lines, custom_image);
+    drawCircles(circles, custom_image);
+    imshow("orginal image", orginal_image);
+    imshow("detected lines & circles", custom_image);
+}
+
+
--- LIDAR/Opencv-test/shapedetector.h
+++ LIDAR/Opencv-test/shapedetector.h
@@ -1,39 +1,122 @@
-#ifndef SHAPEDETECTOR_H
+﻿#ifndef SHAPEDETECTOR_H
 #define SHAPEDETECTOR_H
-#include "highgui/highgui.hpp"
+
 #include "imgproc/imgproc.hpp"
+#include "highgui/highgui.hpp"
+#include "core/core.hpp"
 #include "opencv.hpp"
 #include <iostream>
 #include <fstream>
 
+using namespace std;
+using namespace cv;
 class ShapeDetector
 {
 private:
-    CvScalar circleCenterColor = CV_RGB(0,255,0); // groen
-    CvScalar circumferenceColor = CV_RGB(0,0,255); // groen
-    const int black_pixel = 255;
-    const int white_pixel = 0;
-    const int circleLineType = 8;
-    const int centerThickness = -1;
-    const int circleThickness = 2;
-    const int circleCenterRadius = 3;
-    const int smooth = 7;
-    const int edgeTreshhold = 50;
-    const int resolutionInverseRatio = 1;
-    const int minDistanceCircles = 6;
-    const static int circleCenterTreshhold = 19;
+    CvScalar CIRCLE_CENTER_COLOR = CV_RGB(255,0,0); //! The color for the center of the circle
+    CvScalar CIRCUMFERENCE_COLOR = CV_RGB(0,0,255); //! The color for the circumference of the circle
+    const int CIRCLE_LINE_TYPE = 8; //! the line type of the circumference
+    const int CENTER_THICKNESS = -1;//! the thickness of the point in the middle of the circle
+    const int CIRCLE_THICKNESS = 2; //! the thickness of the circumferenc of the circle
+    const int CIRCLE_CENTER_RADIUS = 3; //! the radius(size) of the point in the middle of the circle
+    const int EDGE_TRESHHOLD = 50; //! the treshhold for detectecting the edges in the image
+    const int RESOLUTION_INVERSERATIO = 1; //! the resolution inverseratio
+    const int MIN_DISTANCE_CIRCLES = 6; //! the minimun distance between the different circles
+    const int CIRCLE_CENTER_TRESHHOLD = 19; //! the treshold for detecting a circle center
+    const int BLACK_PIXEL = 255; //! the value for a black pixel
+    const int WHITE_PIXEL = 0; //! the value for a white pixel
+    const int SMOOTH = 7; //! the value in cvSmooth
+    const int RANGE_CHECK = 15; //! the range for checking inconsistent lines
+    const int CANNY_THRESHHOLD1 = 50; //! the first threshhold used in the canny function
+    const int CANNY_THRESHHOLD2 = 82; //! the second threshhold used in the canny function
+    const double HOUGHLINES_RHO = 1; //! The resolution of the parameter r in pixels. We use 1 pixel.
+    const double HOUGHLINES_THETA = CV_PI/180; //! The resolution of the parameter theta in radians. We use 1 degree (CV_PI/180)
+    const int HOUGHLINES_THRESHHOLD = 20; //! The minimum number of intersections to “detect” a line
+    const double HOUGHLINES_MINLINELENGTH = 50; //! The minimum number of points that can form a line. Lines with less than this number of points are disregarded.
+    const double HOUGHLINES_MAXLINEGAP = 10; //! The maximum gap between two points to be considered in the same line.
+    const CvScalar LINECOLOR = CV_RGB(0,255,0); //! the line color
+    const int THICKNESS = 3; //! the thickness of the line
+
+    //! this function converts a Mat object to a IplImage so the function cvSmooth can be used.
+    /*!
+     @param m_src: the source file
+     @param m_dest: the destination file to save on
+     @param smooth_type: the smooth value
+     @param param1: value1
+     @param param2: value2
+     */
+    bool callCvSmooth(const Mat & m_src, const Mat & m_dest, const int smooth_type, const int param1, const int param2);
+
+    //! the function checks for double lines to prevent unneeded data
+    /*!
+     @param lines: the given lines to check on
+     */
+    void checkLines(vector<Vec4i> & lines);
 
 public:
     ShapeDetector();
     ~ShapeDetector();
-    //This function creates a .jpg file from a .txt file with pixels according to the output paramter.
-    void makeImage(std::string source, std::string output = "output.jpg");
-    //This functions writes all the circle positions and radii to a file according to the outputName paramter
-    void writeCircles(CvSeq * circles, std::string outputName = "output.txt");
-    //This function shows the input image with the detected circles;
-    void showCircles(CvSeq * circles, std::string sourceName = "output.jpg");
+    //! write circles on the
+    /*!
+    This functions writes all the circle positions and radii to a file according to the outputName paramter
+    @param circles: the CvSeq with circles to be written on the console
+    */
+    void writeCirclesToConsole(const CvSeq * circles);
+    //! Draw circles on a given image
+    /*!
+    This function draws all the circles on the given image.
+    @param circles: the CvSeq with circles to be drawn on the image
+    @param image: A reference to the image where the circles should be drawn on
+    */
+    void drawCircles(const CvSeq * circles, Mat & image);
+    //! detect circles in a image
+    /*!
     //This function detect circles and return a CVSEQ pointer with the detected circles
-    CvSeq * detectCircles(std::string sourceName);
+    @param image: The image to detect circles on
+    */
+    CvSeq * detectCircles(const Mat & image);
+    //! create a image from the given .txt source
+    /*!
+    This function creates a .jpg file from a .txt file with pixels and saves it as output.jpg
+    @param image: The image to detect circles on
+    */
+    Mat createImage(const std::string & source);
+
+    //! the function search for lines in the given image and returns the lines
+    /*!
+    @param image: the image to detect lines on
+    */
+    vector<Vec4i> searchLines(const Mat & image);
+
+    //! write the lines container to the console
+    /*!
+    @param lines: the vec4i lines to draw to the console
+    */
+    void writeLinesToConsole(const vector<Vec4i> & lines);
+
+
+    //! draw the lines in a given matrix
+    /*!
+    @param lines: contains the lines to draw;
+    @param final_dest: the image to draw on
+    */
+    void drawLines(const vector<Vec4i> & lines, Mat & final_dest);
+
+    //! write the circle and line objects to the console
+    /*!
+     @param lines: the given lines to write
+     @param circles: the given circles to write
+     */
+    void writeObjectsToConsole(const vector<Vec4i> & lines, const CvSeq * circles);
+
+    //! show the circle and line objects in one window
+    /*!
+     @param lines: the given lines to show
+     @param circles: the given circles to show
+     @param orginal_image: the orginal input image to show
+     @param custom_image: the edited image with the lines and circles to show
+     */
+    void showObjects(const vector<Vec4i> & lines, const CvSeq * circles, const Mat & orginal_image, Mat & custom_image);
 };
 
 #endif // SHAPEDETECTOR_H
