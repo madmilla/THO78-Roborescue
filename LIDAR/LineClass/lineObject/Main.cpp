3573d688e11f009cb0028df2a8de85420ad84f51
Antiserum82 <patrick.schoonheym@student.hu.nl>
2015-04-10 18:37:38 +0200

    Line Class

---
+++ LIDAR/LineClass/lineObject/Main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "Line.h"
+
+std::ostream& operator<<(std::ostream & stream, const Point & point) {
+	stream << '(' << point.x << ' ' << point.y << ')';
+	return stream;
+}
+
+bool test() {
+	std::cout << "running test..." << std::endl << std::endl;
+
+	Point p1{ 50, 50 };
+	Point p2{ 200, 200 };
+
+	Line line(p1, p2);
+
+	std::cout << "line Data:" << std::endl << line.getLine().begin_pos << ' ' << line.getLine().end_pos << std::endl << std::endl;
+
+	p1 = Point{ 100, 100 };
+	p2 = Point{ 400, 400 };
+
+	line.setLine(p1, p2);
+
+	std::cout << "changed line Data:" << std::endl << line.getLine().begin_pos << ' ' << line.getLine().end_pos << std::endl;
+
+	return true;
+}
+
+int main() {
+
+	if (test()) {
+		std::cout << std::endl << "test finished succesfully" << std::endl;	
+		system(" pause");
+		return EXIT_SUCCESS;
+	}
+	else {
+		std::cout << " test failed" << std::endl;
+		return EXIT_FAILURE;
+	}
+}
