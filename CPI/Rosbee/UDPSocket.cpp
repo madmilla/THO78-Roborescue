b851d7e0a09aeb7a9110c237e3721670947a6ba9
hamzaam100 <hamza.aitmessaoud@student.hu.nl>
2015-04-22 10:00:09 +0200

    New git structure

--- CPI/Rosbee/UDPSocket.cpp
+++
@@ -1,7 +0,0 @@
-#include "UDPSocket.hpp"
-void UDPSocket::send(mavlink_message_t * message){
-		server.send(con, message);
-	}
-	void UDPSocket::receive(mavlink_message_t * message){
-		server.receive(message);
-	}
