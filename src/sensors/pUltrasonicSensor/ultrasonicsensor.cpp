c97883751520737570360ec903972fb7838d0ef8
Aesthy <edjekoek@hotmail.com>
2015-04-25 10:17:13 +0200

    ROSBEE - sensor fixes

---
+++ src/sensors/pUltrasonicSensor/ultrasonicsensor.cpp
@@ -0,0 +1,63 @@
+/**
+*               __
+*    _________ / /_  ____  ________  ____________  _____
+*   /___/ __ \/ __ \/ __ \/ ___/ _ \/ ___/ ___/ / / / _ \
+*  / / / /_/ / /_/ / /_/ / /  /  __(__  ) /__/ /_/ /  __/
+* /_/  \____/_.___/\____/_/   \___/____/\___/\__,_/\___/
+*
+*
+* @file ultrasonicsensor.cpp
+* @date Created: 25-4-2015
+* @version 2.1
+*
+* @author Edwin Koek
+*
+* @section LICENSE
+* License: newBSD
+*
+* Copyright © 2015, HU University of Applied Sciences Utrecht.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+#include "ultrasonicsensor.h"
+#include "simpletools.h"
+
+UltraSonicSensor::UltraSonicSensor(int pin):
+pin{pin},
+temperature{22}
+{
+  low(pin);
+}
+
+int UltraSonicSensor::getDistance(){
+  pulse_out(pin,10);
+  int velocity = 331.5 + (0.6 * temperature);
+  int travelTime = pulse_in(pin,1);  
+  int distance = (velocity * travelTime * 100)/(2 * 1000000);
+  if(2 < distance && distance < 330){
+      return distance;
+  }else{
+      return -1;
+  }
+}  
+
+
+void UltraSonicSensor::setTemperature(float temp){
+  temperature = temp;
+}  
