970babefdfaab9710ac426af8f7c41fe55cbcbd6
sqrtroot <sqrtroot@users.noreply.github.com>
2015-05-11 12:44:32 +0200

    Update roborescue.sql

--- src/communication/persistant_storage/roborescue.sql
+++ src/communication/persistant_storage/roborescue.sql
@@ -1,43 +1,65 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+01:00";
+CREATE DATABASE IF NOT EXISTS `robodata` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `robodata`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
-CREATE DATABASE IF NOT EXISTS `roborescue` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `roborescue`;
-
+DROP TABLE IF EXISTS `checkpoint`;
 CREATE TABLE IF NOT EXISTS `checkpoint` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `x` int(11) NOT NULL,
+  `y` int(11) NOT NULL,
+  `z` int(11) NOT NULL,
   `vehicle_id` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `position` point NOT NULL,
+  `map_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `vehicle_id` (`vehicle_id`)
+  KEY `vehicle_id` (`vehicle_id`),
+  KEY `map_id` (`map_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-CREATE TABLE IF NOT EXISTS `line` (
+DROP TABLE IF EXISTS `map`;
+CREATE TABLE IF NOT EXISTS `map` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `point_1` point NOT NULL,
-  `point_2` point NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+DROP TABLE IF EXISTS `object`;
+CREATE TABLE IF NOT EXISTS `object` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `polygon` polygon NOT NULL,
+  `height` int(11) NOT NULL DEFAULT '0',
+  `map_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `qr`;
+CREATE TABLE IF NOT EXISTS `qr` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `x` int(11) NOT NULL,
+  `y` int(11) NOT NULL,
+  `z` int(11) NOT NULL,
+  `map_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `map_id` (`map_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `vehicle`;
 CREATE TABLE IF NOT EXISTS `vehicle` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `depth` int(11) NOT NULL,
-  `width` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `height` int(11) NOT NULL,
+  `width` int(11) NOT NULL,
+  `depth` int(11) NOT NULL,
+  `naam` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 ALTER TABLE `checkpoint`
-  ADD CONSTRAINT `checkpoint_ibfk_1` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `checkpoint_ibfk_2` FOREIGN KEY (`map_id`) REFERENCES `map` (`id`),
+  ADD CONSTRAINT `checkpoint_ibfk_1` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`id`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `object`
+  ADD CONSTRAINT `object_ibfk_1` FOREIGN KEY (`id`) REFERENCES `map` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `qr`
+  ADD CONSTRAINT `qr_ibfk_1` FOREIGN KEY (`map_id`) REFERENCES `map` (`id`);
