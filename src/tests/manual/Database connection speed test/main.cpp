5a1c722677be6f6f366db566b61b2ed6a914af60
Robert Bezem <robert.bezem@gmail.com>
2015-05-17 14:03:23 +0200

    initial commit

---
+++ src/tests/manual/Database connection speed test/main.cpp
@@ -0,0 +1,25 @@
+#include "communication/persistant_storage/databaseconnector.h"
+#include "communication/persistant_storage/point.hpp"
+#include <iostream>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<< ( std::ostream& os, const std::vector<T>& vec ) {
+    for ( auto& el : vec ) {
+        os << el << std::endl;
+    }
+    return os;
+}
+
+
+int main(int argc, char* argv[]) {
+    if(argc < 3){
+        std::cout << "no password specified\nUsage: " << argv[0] <<" [username] [password]\n";
+        return 0;
+    }
+    std::string username = argv[1];
+    std::string password = argv[2];
+    databaseConnector db ( "tcp://127.0.0.1:3306", username , password, "roborescue" );
+	db.getPolygons();
+    return 0;
+}
