9c8af2275466dfb2deb9d094f79bc1d2e184745b
KyleTaylorParkins <kyle.taylorparkins@student.hu.nl>
2015-05-27 16:14:08 +0200

    Rosbee CPI

---
+++ src/vehicle/rosbee/CPI Interface/RosbeeProtocolHandler.cpp
@@ -0,0 +1,8 @@
+#include "RosbeeProtocolHandler.h"
+
+int getTargetLocation(uint8_t message) {
+	//xLocation = message.payload << 4 ;
+	//yLocation = message.Payload << 8;
+	//return sqrt((xLocation^2) + (yLocation^2));
+    return 0;
+}
