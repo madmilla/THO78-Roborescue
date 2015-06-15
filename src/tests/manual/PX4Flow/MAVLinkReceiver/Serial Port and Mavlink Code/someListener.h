f248a5b1628119a329b8a40f2c9b938bf47f6c3d
Daniël van den Berg <danielvandenberg95@gmail.com>
2015-06-15 14:43:20 +0200

    Moved around PX4Flow firmware stuff
    And added a test method as well.

--- src/tests/manual/PX4Flow/MAVLinkReceiver/Serial Port and Mavlink Code/someListener.h
+++ src/tests/manual/PX4Flow/MAVLinkReceiver/Serial Port and Mavlink Code/someListener.h
@@ -1,6 +1,8 @@
 #include "mavlinkSubject.h"
 #include "mavlinkListener.h"
 #include <fstream>
+#include "PX4FlowWrapper.h"
+
 class SomeListener : public MavlinkListener{
 	public:
 		SomeListener();
@@ -8,6 +10,7 @@ class SomeListener : public MavlinkListener{
 		void update(mavlink_message_t* msg) override;
 	private:
 		MavlinkSubject* m;
+		PX4FlowWrapper* pX4FlowWrapper = new PX4FlowWrapper();
 		char img = 0;
 		std::fstream imgFile;
 		int size = 0;
