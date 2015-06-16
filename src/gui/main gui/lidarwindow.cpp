ac2b79617c7e75b5ccdc1844b0a500bdb4b6277d
thijsvt <thijs.vantiem@student.hu.nl>
2015-06-16 10:22:34 +0200

    total fix
    ready now.

--- src/gui/main gui/lidarwindow.cpp
+++ src/gui/main gui/lidarwindow.cpp
@@ -1,38 +1,37 @@
 #include "lidarwindow.h"
 #include "ui_lidarwindow.h"
 
+//! create the lidar and qwidget
 LidarWindow::LidarWindow(Lidar &lidar, QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::lidarwindow()),
     lidar(lidar)
 {
     ui->setupUi(this);
-
     connect(ui->startLidar,SIGNAL(clicked()),this,SLOT(handleButtonLidar()));
     lidarMissionRunning(false);
 }
 
 
-
+//! delete the lidarwindow
 LidarWindow::~LidarWindow(){
     delete ui;
 }
-
-
 
 void LidarWindow::handleButtonLidar(){
     //! this function will handle the buttoncall and down below it will be processed.
     QPushButton * button = static_cast<QPushButton *>(sender());
 
     if(button == nullptr) return;
-
+    //! startlidar has been pressed
     if(button == ui->startLidar){
         try{
             lidar.startLidar();
             lidarMissionRunning(true);
         }
-        catch(...){ //moet een exception gemaakt worden voor het afvangen van geen rpm
-            QMessageBox::critical(this,"Error:, No rpm set.", "mission already started");
+    //! catch if the lidar already has been started.
+        catch(...){
+            QMessageBox::critical(this,"Lidar has already started");
         }
     }
     else if(button == ui->stopLidar){
