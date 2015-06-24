150ae3f7a79a00504b6ff25159be7fdae1c5cda6
coenandriessen <coen.andriessen@student.hu.nl>
2015-06-24 12:30:48 +0200

    virtualRosbee updated with doxygen

--- Src/Controllers/StrategyController/main.cpp
+++ Src/Controllers/StrategyController/main.cpp
@@ -3,11 +3,10 @@ int main(){
 	map Map;
 	QuadCopter copter(Dimension(1, 1), Dimension(3, 3), 1, 1);
 	//ATV atv;
-	virtualRosbee rosbee;
+	virtualRosbee rosbee(5,5);
 	Lidar lidar;
 	StrategyController controller(Map,copter,rosbee,lidar);
 	controller.scanArea();
 	//controller.searchArea();
 	while (1);
-
 }
--- Src/Strategy/Common/virtualRosbee.h
+++ Src/Strategy/Common/virtualRosbee.h
@@ -1,41 +1,109 @@
 /**
- * @file virtualRosbee.h
- * @author Coen Andriessen
- * @version 1.1
- */
+*               __
+*    _________ / /_  ____  ________  ____________  _____
+*   /___/ __ \/ __ \/ __ \/ ___/ _ \/ ___/ ___/ / / / _ \
+*  / / / /_/ / /_/ / /_/ / /  /  __(__  ) /__/ /_/ /  __/
+* /_/  \____/_.___/\____/_/   \___/____/\___/\__,_/\___/
+*
+*
+* @file virtualRosbee.h
+* @date Created: 4/28/2015
+*
+* @author Coen Andriessen
+* @author Jeroen Steendam
+*
+* @section LICENSE
+* License: newBSD
+*
+* Copyright © 2015, HU University of Applied Sciences Utrecht.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
 
 #ifndef ROSBEE_H
 #define ROSBEE_H
-//#include "../../Vehicles/Rosbee/Rosbee.h"
+
 #include <iostream>
-//#include "../../Vehicles/Rosbee/Rosbee.h"
+
 class virtualRosbee
 {
 public:
-    // Constructor of rosbee.
-	virtualRosbee();
-    // Destructor of rosbee.
+	/**
+	* @fn	virtualRosbee(int x, int y);
+	*
+	* @brief	Constructor of the class virtualRosbee.
+	*
+	* @param	int x
+	* @param	int y
+	*/
+	virtualRosbee(int x, int y);
+	/**
+	* @fn	~virtualRosbee();
+	*
+	* @brief	Destructor of the class virtualRosbee.
+	*/
 	~virtualRosbee();
-    // Function to check if the rosbee location is changed.
-    bool isLocationChanged();
-    // Function to get the rosbee location x.
+	/**
+	* @fn	int getRosbeeLocationX();
+	*
+	* @brief	Function that returns the x location of the virtualRosbee.
+	*
+	* @return	int rosbeeLocationX
+	*/
     int getRosbeeLocationX();
-    // Function to get the rosbee location y.
+	/**
+	* @fn	int getRosbeeLocationY();
+	*
+	* @brief	Function that returns the y location of the virtualRosbee.
+	*
+	* @return	int rosbeeLocationY
+	*/
     int getRosbeeLocationY();
-    // Function to get the old rosbee location x.
-    int getOldRosbeeLocationX();
-    // Function to get the old rosbee location y.
-    int getOldRosbeeLocationY();
-    // Function to set the rosbee location y.
+	/**
+	* @fn	void set RosbeeLocationY();
+	*
+	* @brief	Function that set the y location of the virtualRosbee.
+	*/
     void setRosbeeLocationY(int y);
-    // Function to set the rosbee location x.
+	/**
+	* @fn	void set RosbeeLocationX();
+	*
+	* @brief	Function that set the x location of the virtualRosbee.
+	*/
     void setRosbeeLocationX(int x);
+	/**
+	* @fn	void moveTo(int x, int y);
+	*
+	* @brief	Function to move the rosbee to location x and y.
+	*
+	* @param	int x
+	* @param	int y
+	*/
 	void moveTo(int x, int y);
 private:
-    int rosbeeLocationX = 5;
-    int oldRosbeeLocationX;
-    int oldRosbeeLocationY;
-    int rosbeeLocationY = 5;
+	/**
+	* @brief int rosbeeLocationX variable to save rosbee location x.
+	*/
+    int rosbeeLocationX;
+	/**
+	* @brief int rosbeeLocationY variable to save rosbee location y.
+	*/
+    int rosbeeLocationY;
 };
 
 #endif // ROSBEE_H
