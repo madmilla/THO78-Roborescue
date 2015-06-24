db84a8a2532e819ad04359f331e4a28ded79efcc
KyleTaylorParkins <kyle.taylorparkins@student.hu.nl>
2015-06-24 14:45:12 +0200

    LPC1114 for Rosbee
    PI Code to communicate with the LPC1114 used for ADC on the Rosbee.

---
+++ Src/Vehicles/Rosbee/LPC1114/GPIOClass.h
@@ -0,0 +1,33 @@
+// Licensed for public use
+// found on < insert url >, last accessed 24-04-2015
+
+#ifndef GPIO_CLASS_H
+#define GPIO_CLASS_H
+ 
+#include <string>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+/* GPIO Class
+ * Purpose: Each object instantiated from this class will control a GPIO pin
+ * The GPIO pin number must be passed to the overloaded class constructor
+ */
+class GPIOClass
+{
+public:
+    GPIOClass();  // create a GPIO object that controls GPIO4 (default
+    GPIOClass(string x); // create a GPIO object that controls GPIOx, where x is passed to this constructor
+    int export_gpio(); // exports GPIO
+    int unexport_gpio(); // unexport GPIO
+    int setdir_gpio(string dir); // Set GPIO Direction
+    int setval_gpio(string val); // Set GPIO Value (putput pins)
+    int getval_gpio(string& val); // Get GPIO Value (input/ output pins)
+    string get_gpionum(); // return the GPIO number associated with the instance of an object
+private:
+    string gpionum; // GPIO number associated with the instance of an object
+};
+ 
+#endif
---
+++ Src/Vehicles/Rosbee/LPC1114/lpc_com.cpp
@@ -0,0 +1,75 @@
+/**
+*               __
+*    _________ / /_  ____  ________  ____________  _____
+*   /___/ __ \/ __ \/ __ \/ ___/ _ \/ ___/ ___/ / / / _ \
+*  / / / /_/ / /_/ / /_/ / /  /  __(__  ) /__/ /_/ /  __/
+* /_/  \____/_.___/\____/_/   \___/____/\___/\__,_/\___/
+*
+*
+* @file lpc_com.cpp
+* @date Created: 21-6-2015
+*
+* @author Kyle Taylor Parkins
+* @version 1.0
+*
+* @section LICENSE
+* License: newBSD
+*
+* Copyright © 2015, HU University of Applied Sciences Utrecht.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+#include"lpc_com.h"
+
+LPCCom::LPCCom(std::string uartPort, gpioPort) {
+	uart = new LibSerial();
+	gpio = new GPIOClass(gpioPort);
+	portName = uartPort;
+}
+
+LPCCom::~LPCCom(){
+	uart->close();
+	delete uart;
+	gpio->unexport_gpio();
+	delete gpio;
+}
+
+bool LPCCom::init(){
+	gpio->export_gpio(); // export GPIO
+	gpio->setdir_gpio("out"); // GPIO set to output
+	cout << "GPIO operations done" << endl;
+
+	int portResult = uart->open(portName, baudRate);
+	if (portResult != 1){
+		std::cout << "Opening failed, error:" << portResult << std::endl;
+		return false;
+	}else{
+		std::cout << "Opening succesfull" << std::endl;
+		return true;
+	}
+}
+
+void readData(double& batteryPercentage, double& temperature){
+	gpio->setval("1");
+	usleep(20 * 1000);
+	gpio->setval("0");
+
+	uart->read();
+	// zet waarden in de pointers
+}
