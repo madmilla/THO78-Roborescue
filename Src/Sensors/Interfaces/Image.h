f55a53185c802ba4a71041c5f1f838527b6b54a3
FeikoWielsma <feiko.wielsma@student.hu.nl>
2015-06-26 10:08:43 +0000

    Added doxygen to Image.h

--- Src/Sensors/Interfaces/Image.h
+++ Src/Sensors/Interfaces/Image.h
@@ -3,16 +3,30 @@
 
 class Image{
 public:
-	Image(){}
-	
+	/**
+	* Sets the image in this Image class.
+	* Converts from cimg_library::CImg<unsigned char>* to cv::Mat*.
+	*
+	* @param cImg is of type cimg_library::CImg<unsigned char>*.
+	*/
 	void setImage(cimg_library::CImg<unsigned char>* cImg){
 		cImgToMat(cImg);
 	}
 	
+	/**
+	* Sets the image in this Image class.
+	*
+	* @param image is of type cv::Mat*.
+	*/
 	void setImage(cv::Mat* image){
 		this->image = image;
 	}
 	
+	/**
+	* Returns the image contained by this Image class.
+	*
+	* @return cv::Mat*, a pointer to the openCV image.
+	*/
 	cv::Mat* getImage(){
 		return image;
 	}
