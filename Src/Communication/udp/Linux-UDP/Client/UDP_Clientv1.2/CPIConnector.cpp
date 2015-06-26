f3b43868dfaa7b302212a590a9488f0844406aee
Thomson2412 <Thomas.ipod16@gmail.com>
2015-06-26 13:50:06 +0200

    Move cpi connector

--- Src/Communication/udp/Linux-UDP/Client/UDP_Clientv1.2/CPIConnector.cpp
+++ Src/Communication/udp/Linux-UDP/Client/UDP_Clientv1.2/CPIConnector.cpp
@@ -1,37 +1,36 @@
 #include "CPIConnector.h"
 
 //This is an example child class
-CPIConnector::CPIConnector(){}
+CPIConnector::CPIConnector(){
+	lidar = new Lidar("/dev/ttyAMA0");
+	sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, LIDAR_COMMAND_FUNCTIONS::LIDAR_INIT);
+	sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, LIDAR_COMMAND_FUNCTIONS::LIDAR_INIT);
+	sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, LIDAR_COMMAND_FUNCTIONS::LIDAR_INIT);
+	sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, LIDAR_COMMAND_FUNCTIONS::LIDAR_INIT);
+}
 
 void CPIConnector::onMessage(mavlink_message_t & msg){
 
 	mavlink_command_long_t function = decodeCommandLongMessage(msg);
 
 	switch (function.command){
-		case LIDAR_COMMAND_FUNCTIONS::LIDAR_INIT:
+		case LIDAR_COMMAND_FUNCTIONS::START:
+			std::cout << "Start" << std::endl;
+			for (Line l : lidar->start(function.param1, function.param2, function.param3)){
+				message.param1 = 0;
+				message.param2 = static_cast<float>(l.getLine().begin_pos.x);
+				message.param3 = static_cast<float>(l.getLine().begin_pos.y);
+				message.param4 = static_cast<float>(l.getLine().end_pos.x);
+				message.param5 = static_cast<float>(l.getLine().end_pos.y);
+				sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, LIDAR_COMMAND_FUNCTIONS::LINEDATA);
+			}
 
-			systemID = function.param1;
-			sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, COMMAND_DESTINATION::CPI, LIDAR_COMMAND_FUNCTIONS::LIDAR_INIT );
-			break;
-
-		case LIDAR_COMMAND_FUNCTIONS::LIDAR_GETDEVICE:
-
-			sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, COMMAND_DESTINATION::CPI, LIDAR_COMMAND_FUNCTIONS::RPM );
-			break;
-
-		case LIDAR_COMMAND_FUNCTIONS::LINEDATA:
-
-			sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, COMMAND_DESTINATION::CPI, LIDAR_COMMAND_FUNCTIONS::START );
-			break;
-
-		case LIDAR_COMMAND_FUNCTIONS::RPM:
-
-			sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, COMMAND_DESTINATION::CPI, LIDAR_COMMAND_FUNCTIONS::STOP );
+			sendCommand<LIDAR_COMMAND_FUNCTIONS>(message, LIDAR_COMMAND_FUNCTIONS::STOP);
 			break;
 
 		default:
 			function.param1 += 1;
-			sendCommand<LIDAR_COMMAND_FUNCTIONS>(function, COMMAND_DESTINATION::CPI, LIDAR_COMMAND_FUNCTIONS::STOP );
+			sendCommand<LIDAR_COMMAND_FUNCTIONS>(function, LIDAR_COMMAND_FUNCTIONS::STOP );
 			break;
 	}
 }
