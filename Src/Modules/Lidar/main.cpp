3b22ca59ce0f73999c56c5fe438dca1f1c9a81ed
Thomson2412 <Thomas.ipod16@gmail.com>
2015-06-26 13:54:55 +0200

    fix main

--- Src/Modules/Lidar/main.cpp
+++ Src/Modules/Lidar/main.cpp
@@ -1,12 +1,3 @@
-//============================================================================
-// Name        : UDP2.cpp
-// Author      :
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
-
 #include <iostream>
 #include <stdlib.h>
 #include "UDPClient.h"
