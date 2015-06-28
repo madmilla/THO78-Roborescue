7e69c900c5572d46a9238d3c13b80ed80927e763
coenandriessen <coen.andriessen@student.hu.nl>
2015-06-28 14:45:55 +0200

    Doxygen Dimension

--- Src/Strategy/Common/dimension.h
+++ Src/Strategy/Common/dimension.h
@@ -1,15 +1,81 @@
+/**
+*               __
+*    _________ / /_  ____  ________  ____________  _____
+*   /___/ __ \/ __ \/ __ \/ ___/ _ \/ ___/ ___/ / / / _ \
+*  / / / /_/ / /_/ / /_/ / /  /  __(__  ) /__/ /_/ /  __/
+* /_/  \____/_.___/\____/_/   \___/____/\___/\__,_/\___/
+*
+*
+* @file Dimension.h
+* @date Created: 4/28/2015
+* @version 1.0
+*
+* @author Jacob Visser
+*
+* @section LICENSE
+* License: newBSD
+*
+* Copyright © 2015, HU University of Applied Sciences Utrecht.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
 #ifndef DIMENSION_H
 #define DIMENSION_H
 
 class Dimension
 {
 public:
+	/**
+	* @fn	bool operator ==(Dimension d );
+	*
+	* @brief	Operator == for class Dimension.
+	*
+	* @param	d
+	* 
+	* @return 	bool
+	*/
     bool operator ==(Dimension d ){
         if(this->height==d.height&&this->width==d.width){return true;}
         else{return false;}
     }
-    int width,height;
+	/**
+	* @brief width variable. Width of Dimension.
+	*/
+    int width;
+	/**
+	* @brief height variable. Height of Dimension.
+	*/
+	int height;
+	/**
+	* @fn	Dimension(int width,int height);
+	*
+	* @brief	Constructor of the class Dimension.
+	*
+	* @param	width
+	* @param	height
+	*/
     Dimension(int width,int height);
+	/**
+	* @fn	~Dimension();
+	*
+	* @brief	Destructor of the class Dimension.
+	*/
     ~Dimension();
 };
 
