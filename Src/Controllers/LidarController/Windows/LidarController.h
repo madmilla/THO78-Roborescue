192f58143dc5e97c3623e3c6c6b987cb0ce9503a
KjeldPerquin <kjeld.perquin@student.hu.nl>
2015-06-29 15:43:38 +0200

    Boost fix

---
+++ Src/Controllers/LidarController/Windows/LidarController.h
@@ -0,0 +1,20 @@
+#ifndef _LIDAR_CONTROLLER_H
+#define _LIDAR_CONTROLLER_H
+#include <vector>
+#include "roborescueV1/mavlink.h"
+#include "Line.h"
+
+class Lidar;
+class MAVLinkExchanger;
+
+class LidarController
+{
+public:
+	LidarController(Lidar& lidar, MAVLinkExchanger& exchanger);
+	void loop();
+private:
+	Lidar& lidar;
+	MAVLinkExchanger& exchanger;
+	void handleIncomingMessage(mavlink_message_t incomingMessage);
+};
+#endif
