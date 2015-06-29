43f290537fe3707115bed37e6159a7a67a3497f4
yorricklans <yorricklans@hotmail.com>
2015-06-29 20:32:02 +0200

    added some code to lidarcontroller

--- Src/Controllers/LidarController/Windows/LidarController.cpp
+++ Src/Controllers/LidarController/Windows/LidarController.cpp
@@ -21,4 +21,28 @@ void LidarController::loop()
 
 std::vector<Line> LidarController::scanLines()
 {
+	mavlink_message_t message;
+	mavlink_msg_command_long_pack(1, 1, &message, 1, 1, MAV_CMD_LIDAR_START_SCAN, 0, 1, 0, 0, 0, 0, 0, 0);
+	exchanger.enqueueMessage(message);
+	std::vector<Line> lines;
+	while (1)
+	{
+		if (exchanger.receiveQueueSize())
+		{
+			auto incomingMessage = exchanger.dequeueMessage();
+			switch (incomingMessage.msgid)
+			{
+			case MAVLINK_MSG_ID_LIDAR_LINE:
+				std::cout << "RECEIVED MESSAGE: ";
+				Line line{ Point{ mavlink_msg_lidar_line_get_start_X(&incomingMessage), mavlink_msg_lidar_line_get_start_Y(&incomingMessage) },
+					Point{ mavlink_msg_lidar_line_get_end_X(&incomingMessage), mavlink_msg_lidar_line_get_end_Y(&incomingMessage) } };
+				lines.push_back(line);
+				if (mavlink_msg_lidar_line_get_last_line(&incomingMessage) == 0)
+				{
+					return lines;
+				}
+				break;
+			}
+		}
+	}
 }
