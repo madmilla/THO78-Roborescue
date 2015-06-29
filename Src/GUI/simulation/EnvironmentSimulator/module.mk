e1a0dff814dddbae72d5e0aab7a43cb1567f7108
Thomson2412 <Thomas.ipod16@gmail.com>
2015-06-29 16:34:47 +0200

    Module

--- Src/GUI/simulation/EnvironmentSimulator/module.mk
+++ Src/GUI/simulation/EnvironmentSimulator/module.mk
@@ -7,9 +7,9 @@ SRC += Src/GUI/simulation/EnvironmentSimulator/simulatemap.cpp
 SRC += Src/GUI/simulation/EnvironmentSimulator/simulatemapwindow.cpp		
 SRC += Src/GUI/simulation/EnvironmentSimulator/test.cpp		
 		
-UI  += Src/GUI/simulation/EnvironmentSimulator/editmapwindow.ui		
+UI  += Src/GUI/simulation/EnvironmentSimulator/ui_editmapwindow.ui		
 SRC  += Src/GUI/simulation/EnvironmentSimulator/moc_editmapwindow.cpp		
-UI  += Src/GUI/simulation/EnvironmentSimulator/mainwindow.ui		
+UI  += Src/GUI/simulation/EnvironmentSimulator/ui_mainwindow.ui		
 SRC  += Src/GUI/simulation/EnvironmentSimulator/moc_mainwindow.cpp		
-UI  += Src/GUI/simulation/EnvironmentSimulator/simulatemapwindow.ui		
+UI  += Src/GUI/simulation/EnvironmentSimulator/ui_simulatemapwindow.ui		
 SRC  += Src/GUI/simulation/EnvironmentSimulator/moc_simulatemapwindow.cpp
