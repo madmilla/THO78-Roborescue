76c084edd4ec259d2b6cade0740d885078112a93
Feiko Wielsma <feiko.wielsma@student.hu.nl>
2015-06-29 17:50:07 +0000

    Fixed the localisation system on the Pi by adding the old mavlink and serialport implementations

---
+++ Src/Communication/LocalisationCommunication/MavlinkExchanger/module.mk
@@ -0,0 +1,2 @@
+SRC += Src/Communication/LocalisationCommunication/MavlinkExchanger/MAVLinkExchanger.cpp
+SRC += Src/Communication/LocalisationCommunication/MavlinkExchanger/PrioritisedMAVLinkMessage.cpp
