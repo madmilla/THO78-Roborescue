c1ede1a7c2c254a528c77ce07489eed5d6287ed5
KjeldPerquin <kjeld.perquin@student.hu.nl>
2015-06-29 18:09:31 +0200

    More case fixing

--- Src/Strategy/Common/module.mk
+++ Src/Strategy/Common/module.mk
@@ -1,7 +1,7 @@
 SRC +=Src/Strategy/Common/VirtualATV.cpp
-SRC +=Src/Strategy/Common/dimension.cpp
+SRC +=Src/Strategy/Common/Dimension.cpp
 SRC +=Src/Strategy/Common/virtualLidar.cpp
 SRC +=Src/Strategy/Common/VirtualQuadCopter.cpp
-SRC +=Src/Strategy/Common/route.cpp
+SRC +=Src/Strategy/Common/Route.cpp
 SRC +=Src/Strategy/Common/virtualRosbee.cpp
-SRC +=Src/Strategy/Common/waypoint.cpp
+SRC +=Src/Strategy/Common/Waypoint.cpp
