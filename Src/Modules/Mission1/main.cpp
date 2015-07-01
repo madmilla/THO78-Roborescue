629197c5e126e611c34daa38b7a1005925c09069
Owantjaaaa <owen.hoogenboezem@student.hu.nl>
2015-07-01 10:48:21 +0000

    LIDAR demonstration

--- Src/Modules/Mission1/main.cpp
+++ Src/Modules/Mission1/main.cpp
@@ -9,12 +9,60 @@
 
 const std::string LOCAL_IP{"145.89.118.25"};
 void runLocalisationModule();
+void runLidar();
 
 int main(int argc, char *argv[])
 {
 	runLocalisationModule();
     return 0;
 }
+
+
+const int LIDAR_PORT{ 8000 };
+void runLidar()
+{
+	boost::asio::io_service service;
+	TCPConnection c{ service };
+	MAVLinkExchanger exchanger{ &c };
+	c.connect(8000);
+	
+	std::thread exchangerThread{ &MAVLinkExchanger::loop, std::ref(exchanger) };
+	std::thread lineReceiveThread{ [&exchanger]()
+	{
+		while(1)
+		{
+			while (exchanger.receiveQueueSize())
+			{
+				auto line = exchanger.dequeueMessage();
+				std::cout << "Line: " 
+				<< mavlink_msg_lidar_line_get_start_X(&line) << ','
+				<< mavlink_msg_lidar_line_get_start_Y(&line) << " tot " 
+				<< mavlink_msg_lidar_line_get_end_X(&line) << ','
+				<< mavlink_msg_lidar_line_get_end_Y(&line) << std::endl;
+			}
+		}
+	}};
+	std::thread startInputThread { [&exchanger]()
+	{
+		mavlink_message_t message;
+		mavlink_msg_command_long_pack(1, 1, &message, 1,1, MAV_CMD_LIDAR_START_SCAN, 0,1, 0,0,0,0,0,0);
+		while(1)
+		{
+			char c;
+			std::cin >> c;
+			if(c != ' ')
+			{
+				std::cout << "Sent message" << std::endl;
+				exchanger.enqueueMessage(message);
+			}
+		}
+	}};
+	exchangerThread.detach();
+	lineReceiveThread.detach();
+	startInputThread.detach();
+	service.run();
+}
+
 
 const std::string QUADCOPTER_COM_PORT{ "COM4" };
 const int QUADCOPTER_BAUDRATE{ 57600};
